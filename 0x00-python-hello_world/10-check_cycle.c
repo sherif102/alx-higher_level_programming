8d44e954c77fa3311bf302ddce5c29beace35e0c
hyperbayse <hyperbayse@gmail.com>
2024-09-05 16:07:33 +0100

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
         listint_t *slow = list;
         listint_t *fast;
 
-        if (list->next == NULL)
+        if (list->next == NULL || list == NULL)
                 return (0);
 
 	fast = slow;
