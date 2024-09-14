5f3d33a6e74d5ec72c6a68905a51806af6e769bd
hyperbayse <hyperbayse@gmail.com>
2024-09-15 00:27:53 +0100

    update

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if (ord(c) >= 97 <= 123):
+            b = ord(c) - 32
+        else:
+            b = ord(c)
+        
+        print("{}".format(chr(b)), end='')
+    print("{}".format(''))
+
+uppercase("Best School 98 Battery street")
