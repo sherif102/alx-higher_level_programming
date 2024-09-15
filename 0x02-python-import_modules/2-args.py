41037c247e797a33662dfdfc03a5488600f30a7c
hyperbayse <hyperbayse@gmail.com>
2024-09-15 23:39:16 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-argNumber = sys.argv
-argLen = len(argNumber) - 1
-if argLen == 0:
+    from sys import argv
+if len(argv) == 1:
     print("0 arguments.")
-elif argLen == 1:
+elif len(argv) == 2:
     print("1 argument:")
-    print("1: {}".format(argNumber[1]))
+    print("1: {}".format(argv[1]))
 else:
-    print("{} arguments:".format(argLen))
-    for i in range(1, argLen + 1):
-        print("{}: {}".format(i, argNumber[i]))
+    print("{} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
---
+++ 0x02-python-import_modules/te.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print("Here we go")
+for x in range(10):
+    print(x)
+print(dir())
