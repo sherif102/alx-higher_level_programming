ebbd2a9e1144b5fd39c6f68ece68954f7f8446ed
hyperbayse <hyperbayse@gmail.com>
2024-09-15 01:00:25 +0100

    update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def fizzBuzz():
+def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0 and i % 5 == 0):
             print("FizzBuzz", end='')
@@ -11,4 +11,3 @@ def fizzBuzz():
             print("{}".format(i), end='')
         if (i < 100):
             print(' ', end='')
-
