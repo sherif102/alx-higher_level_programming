1d74222de6039a977a43531bb179e104e5614124
hyperbayse <hyperbayse@gmail.com>
2024-09-16 09:28:14 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
@@ -11,4 +10,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
-
