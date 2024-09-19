75ae47feb4ee044f791c5833240a13baf7e75f1a
hyperbayse <hyperbayse@gmail.com>
2024-09-19 08:00:52 +0100

    update

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     if my_list is None:
-        return my_list
+        return None
     for item in my_list:
         print("{:d}".format(item))
