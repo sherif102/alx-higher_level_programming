3f76a2c153beac6f64a3f079c6edbb95f8f312d2
hyperbayse <hyperbayse@gmail.com>
2024-09-19 10:10:56 +0100

    update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 1:
-        c = 0
+        a, c = tuple_a[0], 0
     if len(tuple_b) == 1:
-        d = 0
+        b, d = tuple_b[0], 0
     if len(tuple_a) == 0:
         a, c = 0, 0
     if len(tuple_b) == 0:
         b, d = 0, 0
-    
+
     if len(tuple_a) > 1:
         a, c = tuple_a[0], tuple_a[1]
     if len(tuple_b) > 1:
         b, d = tuple_b[0], tuple_b[1]
-    
+
     result = (a + b, c + d)
     return result
