99221bb8f58eeeacfb2a66b86817db5401f397da
hyperbayse <hyperbayse@gmail.com>
2024-09-23 06:50:46 +0100

    matrix refactoring

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sq_matrix = []
-    for x in matrix:
-        sq_matrix.append(list(map(lambda y: y ** 2, x)))
-    return sq_matrix
+    return [list(map(lambda y: y ** 2, x)) for x in matrix]
