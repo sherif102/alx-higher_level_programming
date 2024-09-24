a0a6b2347bc280eb206957e77310c5bbd766eb03
hyperbayse <hyperbayse@gmail.com>
2024-09-24 16:54:43 +0100

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,13 @@ def roman_to_int(roman_string):
     else:
         rs = roman_string + 'O'
         rv = {'O': 0,
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000}
+              'I': 1,
+              'V': 5,
+              'X': 10,
+              'L': 50,
+              'C': 100,
+              'D': 500,
+              'M': 1000}
         rk = list(rv)
         result = 0
         for x in range(0, len(rs) - 1):
