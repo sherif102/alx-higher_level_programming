2613e92743a9e75d059fae1ac5e2990386f34f50
hyperbayse <hyperbayse@gmail.com>
2024-09-24 16:39:00 +0100

    update

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rs = roman_string + 'O'
+    rv = {'O': 0,
+          'I': 1,
+          'V': 5,
+          'X': 10,
+          'L': 50,
+          'C': 100,
+          'D': 500,
+          'M': 1000}
+    rk = list(rv)
+    result = 0
+    for x in range(0, len(rs) - 1):
+        if rs[x] in rk:
+            if rv[rs[x]] < rv[rs[x + 1]]:
+                result -= rv[rs[x]]
+            else:
+                result += rv[rs[x]]
+        else:
+            return 0
+    return result
