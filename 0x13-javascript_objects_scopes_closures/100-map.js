ec92162d66c2db1cd7256f226e7318d8369a41f2
hyperbayse <hyperbayse@gmail.com>
2024-09-27 14:44:10 +0100

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const list = require('./100-data').list;
-console.log(list);
-
-const map = list.map((x, index) => x * index);
-console.log(map);
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-//  this defines a square and inherits from Rectangle of 4-rectangle.js
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-};
