e7810dbc3542e4b12c08f295685c605266601bc2
hyperbayse <hyperbayse@gmail.com>
2024-09-27 14:34:14 +0100

    javascript update

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const list = require('./100-data').list;
+console.log(list);
+
+const map = list.map((x, index) => x * index);
+console.log(map);
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+// Defines a rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let j = 0;
+      for (; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+
+      if (j === this.width) {
+        console.log('');
+      }
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
