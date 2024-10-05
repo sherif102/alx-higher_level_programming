47f3a444b1008b5d708f95e7c9c81532714ee44b
hyperbayse <hyperbayse@gmail.com>
2024-10-05 14:27:39 +0100

    correction check

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as e:
+    except Exception as e:
         print("Exception: {}".format(e.args[0]))
         return False
