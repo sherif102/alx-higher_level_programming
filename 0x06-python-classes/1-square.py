9908f5fd8379c8b6dd508da95d30736411502c84
hyperbayse <hyperbayse@gmail.com>
2024-10-07 09:13:39 +0100

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" This module defines a class type - square - with a private attribute - size - """
+""" This module defines a class type - square -
+with a private attribute - size - """
 
 
 class Square:
-    """ A class with attribute - size - created at the time of instantiation """
+    """ A class with attribute - size - created
+    at the time of instantiation """
     def __init__(self, size):
         """ Initialization of -size- attribute """
         self.__size = size
