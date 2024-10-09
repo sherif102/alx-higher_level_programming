7e2745280560f587d16fccd6bc75b2fbc8a4eb62
hyperbayse <hyperbayse@gmail.com>
2024-10-09 16:59:25 +0100

    tdd update

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This modules is the definition of a MagicClass """
+
+
+class MagicClass:
+    """ defines a magicclass with initialization """
+
+
+
+    def __init__(self, radius):
+        self._MagicClass__radius = radius
+
+    # uncompleted
