222ddce2addda3e43e3c5026d5399790f535cf7b
hyperbayse <hyperbayse@gmail.com>
2024-10-10 08:36:28 +0100

    update

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """ divides a matrix by a divisor """
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if len(matrix) != 0:
+        if not isinstance(matrix[0], list):
+            raise TypeError("matrix must be a matrix"
+                            "(list of lists) of integers/floats")
+        row = len(matrix[0])
+        for m in matrix:
+            for i in m:
+                if not isinstance(i, (int, float)):
+                    raise TypeError("matrix must be a matrix"
+                                    "(list of lists) of integers/floats")
+            if len(m) != row:
+                raise TypeError("Each row of the matrix must"
+                                "have the same size")
+    return [[round(i / div, 2) for i in m] for m in matrix]
