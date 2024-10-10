856cade80be442499e3a371ba32ed1d18367d4bd
hyperbayse <hyperbayse@gmail.com>
2024-10-10 13:48:09 +0100

    update unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module: 6-max_integer_test
+Author: Sheriff Abdulfatai
+
+Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ tests the funtion max_integer """
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([7, 8, 2]), 8)
+        self.assertEqual(max_integer([7, 8, 20]), 20)
+        self.assertEqual(max_integer([17, 8, 2]), 17)
+        self.assertEqual(max_integer(['a', 'y', 't']), 'y')
+        with self.assertRaises(TypeError):
+            max_integer([7, 8, '2'])
+        with self.assertRaises(TypeError):
+            max_integer(6)
+        self.assertEqual(max_integer(), None)
+        with self.assertRaises(TypeError):
+            max_integer(6, 2)
