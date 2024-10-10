b1d3370c3441a18e2d08e56d00738ab087440171
hyperbayse <hyperbayse@gmail.com>
2024-10-10 09:08:17 +0100

    update

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" the module 3-say_my_name """
+
+
+def say_my_name(first_name, last_name=""):
+    """ prints the full_name """
+    if first_name is None:
+        first_name = "(null)"
+    if last_name is None:
+        last_name = "(null)"
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
