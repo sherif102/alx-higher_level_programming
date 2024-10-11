9e41f40df048f74f31f5e136bfc1ed4a7299d5d5
hyperbayse <hyperbayse@gmail.com>
2024-10-11 22:25:04 +0100

    update

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -5,7 +5,80 @@
 
 
 def matrix_mul(m_a, m_b):
-    """ multiplies m_a and m_b matrices """
+    """ multiplies m_a and m_b matrices
+
+    --------------------------------------------
+    # correct input
+    >>> matrix_mul([[3, 1, 2], [0, 2, 1], [1, 3, 2]], [[0, 1, 1], [2, 0, 1], [1, 2, 0]])
+    [[4, 7, 4], [5, 2, 2], [8, 5, 4]]
+
+    # m_a is not a list
+    >>> matrix_mul('a', [[3, 1, 2], [0, 2, 1], [1, 3, 2]])
+    Traceback (most recent call last):
+        ...
+    TypeError: m_a must be a list
+
+    # m_b is not a list
+    >>> matrix_mul([[3, 1, 2], [0, 2, 1], [1, 3, 2]], 4)
+    Traceback (most recent call last):
+        ...
+    TypeError: m_b must be a list
+
+    # m_a is not a list of lists
+    >>> matrix_mul([[3, 1, 2], [0, 2, 1], 4], [[0, 1, 1], [2, 0, 1], [1, 2, 0]])
+    Traceback (most recent call last):
+        ...
+    TypeError: m_a must be a list of lists
+
+    # m_b is not a list of lists
+    >>> matrix_mul([[3, 1, 2], [0, 2, 1], [1, 3, 2]], ['c', [2, 0, 1], [1, 2, 0]])
+    Traceback (most recent call last):
+        ...
+    TypeError: m_b must be a list of lists
+
+    # m_a is empty
+    >>> matrix_mul([[]], [[0, 0, 0], [2, 0, 1], [1, 2, 0]])
+    Traceback (most recent call last):
+        ...
+    ValueError: m_a can't be empty
+
+    # m_b is empty
+    >>> matrix_mul([[3, 1, 2], [0, 2, 1], [1, 3, 2]], [])
+    Traceback (most recent call last):
+        ...
+    ValueError: m_b can't be empty
+
+    # one element of m_a is not an int or float
+    >>> matrix_mul([[1, 0, 2], [3, 5j, 2], [0, 2, 1]], [[2, 1, 2], [3, 0, 2], [1, 3, 2]])
+    Traceback (most recent call last):
+        ...
+    TypeError: m_a should contain only integers or floats
+
+    # one element of m_b is not an int or float
+    >>> matrix_mul([[1, 0, 2], [3, 0, 2], [0, 2, 1]], [[2, 1, 2], [3, 0, 2], [1, 3, 's']])
+    Traceback (most recent call last):
+        ...
+    TypeError: m_b should contain only integers or floats
+
+    # m_a is not a rectangle
+    >>> matrix_mul([[1, 0, 2], [3, 2, 2], [0, 1]], [[2, 1, 2], [3, 0, 2], [1, 3, 2]])
+    Traceback (most recent call last):
+        ...
+    TypeError: each row of m_a must be of the same size
+
+    # m_b is not a rectangle
+    >>> matrix_mul([[1, 0, 2], [3, 2, 2], [0, 1, 1]], [[1, 2], [3, 0, 2], [1, 3, 2]])
+    Traceback (most recent call last):
+        ...
+    TypeError: each row of m_b must be of the same size
+
+    # m_a rows != m_b columns
+    >>> matrix_mul([[1, 0], [3, 2, 2], [0, 1]], [[2, 1, 2], [3, 0, 2], [1, 3, 2]])
+    Traceback (most recent call last):
+        ...
+    ValueError: m_a and m_b can't be multiplied
+
+    """
 
     result = []
 
@@ -13,10 +86,10 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    # if not isinstance(m_a[0], list):
-    #     raise TypeError("m_a must be a list of lists")
-    # if not isinstance(m_b[0], list):
-    #     raise TypeError("m_b must be a list of lists")
+    if not isinstance(m_a[0], list):
+        raise TypeError("m_a must be a list of lists")
+    if not isinstance(m_b[0], list):
+        raise TypeError("m_b must be a list of lists")
     if len(m_a) == 0 or len(m_a[0]) == 0:
         raise ValueError("m_a can't be empty")
     if len(m_b) == 0 or len(m_b[0]) == 0:
@@ -47,7 +120,7 @@ def matrix_mul(m_a, m_b):
                                     "only integers or floats")
                 if len(m_a[xr]) != len(m_a[0]):
                     raise TypeError("each row of m_a must be of the same size")
-                if len(m_b[mbc]) != len(m_b[0]):
+                if len(m_b[xc]) != len(m_b[0]):
                     raise TypeError("each row of m_b must be of the same size")
                 cal += m_a[xr][xc] * m_b[xc][mbc]
             res.append(cal)
@@ -55,3 +128,8 @@ def matrix_mul(m_a, m_b):
         result.append(res)
         res = []
     return result
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
