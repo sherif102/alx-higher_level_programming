e5959b22be7e8459dd0c8b40049f1d2b169e0660
hyperbayse <hyperbayse@gmail.com>
2024-10-11 21:57:09 +0100

    update

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -13,10 +13,10 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    if not isinstance(m_a[0], list):
-        raise TypeError("m_a must be a list of lists")
-    if not isinstance(m_b[0], list):
-        raise TypeError("m_b must be a list of lists")
+    # if not isinstance(m_a[0], list):
+    #     raise TypeError("m_a must be a list of lists")
+    # if not isinstance(m_b[0], list):
+    #     raise TypeError("m_b must be a list of lists")
     if len(m_a) == 0 or len(m_a[0]) == 0:
         raise ValueError("m_a can't be empty")
     if len(m_b) == 0 or len(m_b[0]) == 0:
@@ -31,22 +31,25 @@ def matrix_mul(m_a, m_b):
     res = []
     cal = 0
 
-    for xr in range(len(m_a[0])):
+    for xr in range(len(m_a)):
         for mbc in range(len(m_b[0])):
-            if len(m_a[xr]) != len(m_a[0]):
-                    raise TypeError("each row of m_a must be of the same size")
-            if len(m_b[mbc]) != len(m_b[0]):
-                raise TypeError("each row of m_b must be of the same size")
-            
             for xc in range(len(m_b)):
+                if not isinstance(m_a[xr], list):
+                    raise TypeError("m_a must be a list of lists")
+                if not isinstance(m_b[xc], list):
+                    raise TypeError("m_b must be a list of lists")
+
                 if not isinstance(m_a[xr][xc], (int, float)):
                     raise TypeError("m_a should contain "
                                     "only integers or floats")
                 if not isinstance(m_b[xc][mbc], (int, float)):
                     raise TypeError("m_b should contain "
                                     "only integers or floats")
+                if len(m_a[xr]) != len(m_a[0]):
+                    raise TypeError("each row of m_a must be of the same size")
+                if len(m_b[mbc]) != len(m_b[0]):
+                    raise TypeError("each row of m_b must be of the same size")
                 cal += m_a[xr][xc] * m_b[xc][mbc]
-                
             res.append(cal)
             cal = 0
         result.append(res)
