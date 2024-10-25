6facb8d6f6199163d57b603c087323753575a17e
hyperbayse <hyperbayse@gmail.com>
2024-10-25 18:23:54 +0100

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: 6-base_geometry.py
+Author: Sheriff Abdulfatai
+"""
+
+
+class BaseGeometry():
+    """ the base class """
+    def area(self):
+        """ raises exception """
+        raise Exception("area() is not implemented")
