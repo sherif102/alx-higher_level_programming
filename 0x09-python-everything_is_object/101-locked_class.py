ace7d5f6f75e513986acf371246bbd7c0f7845d7
hyperbayse <hyperbayse@gmail.com>
2024-10-25 15:57:53 +0100

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 class LockedClass:
     __slots__ = ["first_name"]
+
+
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    print(lc.__dict__)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 0-lookup.py
+Author: Sheriff Abdulfatai
+"""
+
+
+def lookup(obj):
+    """ returns the list of objects attributes and methods """
+    return dir(obj)
