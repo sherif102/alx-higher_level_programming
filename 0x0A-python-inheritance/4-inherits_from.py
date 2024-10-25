ba3f014c7f1093653feaf633d4e9252d620b0731
hyperbayse <hyperbayse@gmail.com>
2024-10-25 18:17:20 +0100

    update

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 4-inherits_from.py
+Author: Sheriff Abdulfatai
+"""
+
+
+def inherits_from(obj, a_class):
+    """ check if obj inherits from a_class """
+    return isinstance(obj, a_class) and type(obj) != a_class
