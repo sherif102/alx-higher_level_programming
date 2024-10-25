1282676da3fc98aa523e1e6ac73bdc3a5d5bc6cd
hyperbayse <hyperbayse@gmail.com>
2024-10-25 17:27:39 +0100

    update

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 1-my_list.py
+Author: Sheriff Abdulfatai
+"""
+
+
+def is_same_class(obj, a_class):
+    """ returnes true if obj is instance of class """
+    return isinstance(a_class, obj)
