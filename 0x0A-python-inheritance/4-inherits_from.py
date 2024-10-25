62e33a5b72a8ca293447e69019adc76d7724ccc3
hyperbayse <hyperbayse@gmail.com>
2024-10-25 18:17:54 +0100

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ Author: Sheriff Abdulfatai
 
 def inherits_from(obj, a_class):
     """ check if obj inherits from a_class """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
