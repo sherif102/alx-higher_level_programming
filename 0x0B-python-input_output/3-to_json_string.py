053d926a5b9e1a71af554f300f67063ddc105ec7
hyperbayse <hyperbayse@gmail.com>
2024-10-26 22:09:00 +0100

    update

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module: 0-read_file.py
+Author: Sheriff Abdulfatai
+"""
+
+
+def to_json_string(my_obj):
+    """ send data to json string """
+    import json
+    return json.dumps(my_obj)
