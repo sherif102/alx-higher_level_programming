28079801a4299bb70778f60631146266002b5dc4
hyperbayse <hyperbayse@gmail.com>
2024-10-26 21:23:42 +0100

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module: 0-read_file.py
+Author: Sheriff Abdulfatai
+"""
+
+
+def read_file(filename=""):
+    """ read file and print to stdout """
+    with open(filename) as file:
+        print(file.read())
