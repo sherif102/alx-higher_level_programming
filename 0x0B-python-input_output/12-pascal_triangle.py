58bd24f34164cc025def2e88438ad479d3406e87
hyperbayse <hyperbayse@gmail.com>
2024-10-28 00:54:13 +0100

    update

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,21 +5,22 @@ Author: Sheriff Abdulfatai
 """
 
 
-def permutate(x):
-    """ permutation """
-    y = 1
-    for i in range(1, x + 1,):
-        y *= i
-    return y
-
-
-def combine(n, k):
-    """ combination """
-    return permutate(n) // (permutate(k) * permutate(n - k))
-
-
 def pascal_triangle(n):
     """ define pascal triangle of n size """
+    def permutate(x):
+        """ permutation """
+        y = 1
+        for i in range(1, x + 1,):
+            y *= i
+        return y
+
+    def combine(n, k):
+        """ combination """
+        return permutate(n) // (permutate(k) * permutate(n - k))
+
+    if n <= 0:
+        return []
+
     result = []
     for f_number in range(n + 1):
         data = []
