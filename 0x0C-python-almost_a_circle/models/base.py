36a959b7d871aeb92b482ff6c11b443d0d4da658
hyperbayse <hyperbayse@gmail.com>
2024-10-31 15:49:59 +0100

    update, almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: base.py
+Author: Shriff Abdulfatai
+"""
+
+
+class Base:
+    """ defines the id of every object """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialization"""
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
