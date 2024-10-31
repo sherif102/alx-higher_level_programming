7fc85d214dc17428202f668a806f172e1d9c4108
hyperbayse <hyperbayse@gmail.com>
2024-11-01 00:00:09 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,4 +101,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """ define the string representation """
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " \
+            "{self.__width}/{self.__height}"
