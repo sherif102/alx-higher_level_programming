6212b5dec89883a82ba4dc9da196a8bea2b21ebe
hyperbayse <hyperbayse@gmail.com>
2024-10-31 22:39:30 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,16 +5,12 @@ Author: Shriff Abdulfatai
 """
 
 
-Base = __import__('base').Base
-""" Base class that monitor the id of each object """
-
-
+from models.base import Base
 class Rectangle(Base):
-    """ rectangle class inheriting from base and initializing the base values with super """
+    """ rectangle class inheriting from base """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ initialization with super id """
-        super().__init__(id=None)
-        """ initianization of the base model """
+        """initialization with super id"""
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
@@ -22,40 +18,36 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ function that get width """
+        """ get width """
         return self.__width
-
     @width.setter
     def width(self, value):
-        """ function that set width value """
+        """ set width value """
         self.__width = value
 
     @property
     def height(self):
-        """ function that get width """
+        """ get width """
         return self.__height
-
     @height.setter
     def height(self, value):
-        """ function that set width value """
+        """ set width value """
         self.__height = value
 
     @property
     def x(self):
-        """ function that get width """
+        """ get width """
         return self.__x
-
     @x.setter
     def x(self, value):
-        """ function that set width value """
+        """ set width value """
         self.__x = value
 
     @property
     def y(self):
-        """ function that get width """
+        """ get width """
         return self.__y
-
     @x.setter
     def y(self, value):
-        """ function that set width value """
+        """ set width value """
         self.__y = value
