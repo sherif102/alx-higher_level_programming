fa4feac28beddc8cab88f6925494b2b0b3e92eff
hyperbayse <hyperbayse@gmail.com>
2024-10-31 16:14:48 +0100

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,7 @@ class Base:
 
     def __init__(self, id=None):
         """initialization"""
-        if id == None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
