6f9770421e8e5ec603f7432806502fca446032a8
hyperbayse <hyperbayse@gmail.com>
2024-12-08 16:48:37 +0100

    update

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- retrieve the top 3 cities with hot temperature from temperatures
 SELECT city, avg(value) as avg_temp
 FROM temperatures 
-WHERE `month` = 'July' OR `month` = 'August'
+WHERE month IN (7, 8)
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
