2e922e97b842cbfa4a90c4610449061a1c6195ef
hyperbayse <hyperbayse@gmail.com>
2024-12-08 11:26:48 +0100

    update group and count

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list the number of records in the table with teh same score
-SELECT score, sum(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, count(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
