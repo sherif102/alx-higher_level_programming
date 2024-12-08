d4dcba1669a49737694e4a7df021f22e7d8937f2
hyperbayse <hyperbayse@gmail.com>
2024-12-08 09:36:26 +0100

    update

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all record >= 10 in table
-SELECT sore, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
