161c240b31c2853072912bd3e2bc242c0a3d1ac9
hyperbayse <hyperbayse@gmail.com>
2024-12-11 00:45:22 +0100

    update

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- create database 'hbtn_0d_2' and use the user 'user_0d-2';
+-- create database 'hbtn_0d_2' and user 'user_0d-2';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
