3cc1245d2544e6247151e7d19df427cd8e36f3e2
hyperbayse <hyperbayse@gmail.com>
2024-12-11 19:19:52 +0100

    update

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,4 @@
 -- use db hbtn_0d_tvshows to list all genres of Dexter
-USE hbtn_0d_tvshows;
 SELECT tg.name FROM tv_genres tg
 JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 JOIN tv_shows ts ON ts.id = tsg.show_id
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,4 @@
 -- select all comedy shows from hbtn_0d_tvshows
-USE hbtn_0d_tvshows
 SELECT ts.title FROM tv_shows ts
 JOIN tv_show_genres tsg ON tsg.show_id = ts.id
 JOIN tv_genres tg ON tg.id = tsg.genre_id
