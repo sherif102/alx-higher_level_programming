eaf7a0095ff90d24d104161da04fe464408a7d88
hyperbayse <hyperbayse@gmail.com>
2024-12-11 17:56:41 +0100

    UPDATE

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- count all the records linked to each genre
-SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows from tv_genres tg
+SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows FROM tv_genres tg
 JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
