96f05dd4345c729b6cce9dbbb81cdfdc996701f8
hyperbayse <hyperbayse@gmail.com>
2025-01-11 17:37:22 +0100

    correction

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 let argMessage;
-if (argv[2] == undefined) argMessage = 'No argument';
+if (argv[2] === undefined) argMessage = 'No argument';
 else argMessage = argv[2];
 console.log(argMessage);
