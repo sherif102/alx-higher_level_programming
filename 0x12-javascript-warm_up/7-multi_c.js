5fd8f00c699ddaa1d1a130ce2f79f3247149bed3
hyperbayse <hyperbayse@gmail.com>
2025-01-12 08:02:24 +0100

    done 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (isNaN(argv[2]) || argv[2] === undefined) console.log('Missing number of occurrences');
+let count = 0;
+while (count < Number(argv[2])) {
+  console.log('C is fun')
+  count++;
+}
