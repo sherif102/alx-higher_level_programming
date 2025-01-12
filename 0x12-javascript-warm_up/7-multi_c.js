9b3dc64d25391d9b31b45c6437df41e335d468ef
hyperbayse <hyperbayse@gmail.com>
2025-01-12 08:02:59 +0100

    update on 7*

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,6 +3,6 @@ const { argv } = require('node:process');
 if (isNaN(argv[2]) || argv[2] === undefined) console.log('Missing number of occurrences');
 let count = 0;
 while (count < Number(argv[2])) {
-  console.log('C is fun')
+  console.log('C is fun');
   count++;
 }
