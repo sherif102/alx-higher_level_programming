9622bcc86aba63eae2a61a5ff3c6ff5ea351c5db
hyperbayse <hyperbayse@gmail.com>
2025-01-14 21:23:43 +0100

    done 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let width = '';
+    for (let i = 0; i < this.width; i++) width = width + 'X';
+    for (let i = 0; i < this.height; i++) console.log(width);
+  }
+
+  rotate () {
+    let copy = this.width;
+    this.width = this.height;
+    this.height = copy;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
