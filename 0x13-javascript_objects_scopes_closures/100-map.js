44454b7fd9042c1f3f5ac45fbf9941cb901a9bb3
hyperbayse <hyperbayse@gmail.com>
2025-01-17 16:00:49 +0100

    filename update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./test').list;
+const list = require('./100-data').list;
 const nList = list.map((x, counter = 0) => x * counter++);
 console.log(list);
 console.log(nList);
