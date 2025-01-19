58150ecd671d01636282c735447ae77dfc8347a3
hyperbayse <hyperbayse@gmail.com>
2025-01-19 16:53:04 +0100

    done 1-filter_states.py

--- 0x0F-python-object_relational_mapping/script.sql
+++ 0x0F-python-object_relational_mapping/script.sql
@@ -1,10 +1,9 @@
--- Database + tables to test
-DROP DATABASE IF EXISTS test_0;
-CREATE DATABASE IF NOT EXISTS test_0;
-USE test_0;
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
 CREATE TABLE IF NOT EXISTS states ( 
     id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
-INSERT INTO states (name) VALUES ("California"), ("Arizona");
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
