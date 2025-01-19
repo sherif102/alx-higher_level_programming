f7bd36c6569d692b6dab77ff1a8b09b80d00117f
hyperbayse <hyperbayse@gmail.com>
2025-01-20 00:12:21 +0100

    done 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module: 0-select_states.py
+Author: Sheriff Abdulfatai
+"""
+
+
+def screener(data):
+    """ screens the data input into the database statement """
+    lookup = ['"', "'", ';']
+    screened = []
+    for x in data:
+        if x in lookup:
+            screened.append('\\')
+        screened.append(x)
+    return ''.join(screened)
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3])
+    except Exception as e:
+        print(f"Error: {e.args[1]}")
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states "
+                "WHERE BINARY name = '{}' "
+                "ORDER BY id".format(screener(argv[4])))
+    for row in cur.fetchall():
+        print(row)
