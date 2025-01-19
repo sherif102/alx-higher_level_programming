4ef6b7b842d5a03a64d491dfd6e8615dab625af1
hyperbayse <hyperbayse@gmail.com>
2025-01-19 17:49:25 +0100

    done 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module: 0-select_states.py
+Author: Sheriff Abdulfatai
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3])
+    except Exception as e:
+        print(f"Error: {e.args[1]}")
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name like '%s' ORDER BY id", (argv[4],))
+    for row in cur.fetchall():
+        print(row)
