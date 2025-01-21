01b980afa258ffcd5d06bc180e72b55d29954eb9
hyperbayse <hyperbayse@gmail.com>
2025-01-21 19:53:41 +0100

    correction

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,8 +11,9 @@ from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
+
 class State(Base):
     """ ORM class for state table """
-    __table__ = 'states'
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
