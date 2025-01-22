71c3a8cce71d7e216ac66c0c1706fe79fcbc55a3
hyperbayse <hyperbayse@gmail.com>
2025-01-22 08:57:44 +0100

    done 11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+"""
+Module: 11-model_state_insert.py
+Author: Sheriff Abdulfatai
+"""
+
+if __name__ == "__main__":
+    from sys import argv as a
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(f"mysql+mysqldb://{a[1]}:{a[2]}@localhost/{a[3]}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name='Louisiana'))
+    session.commit()
+    query = session.query(State).filter_by(name='Louisiana').first()
+    print(query.id)
