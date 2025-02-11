0118f534fb5c190ca4378e4778cc010672147981
hyperbayse <hyperbayse@gmail.com>
2025-02-11 11:26:40 +0100

    correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,7 +13,7 @@ def find_peak(list_of_integers):
         return None
     if list_len < 3 and list_len > 0:
         return sorted(list_of_integers)[list_len]
-    while start <= list_len and list_len > 1:
+    while start < list_len and list_len > 1:
         peak = start
         if list_of_integers[peak] > list_of_integers[peak - 1] and\
                 list_of_integers[peak] > list_of_integers[peak + 1]:
