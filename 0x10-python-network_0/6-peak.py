76cf188f17d92b5a97cf1f681921b644a7a9c8eb
hyperbayse <hyperbayse@gmail.com>
2025-02-11 13:48:42 +0100

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,6 +7,8 @@ Author: Sheriff Abdulfatai
 
 def find_peak(list_of_integers):
     """finds the peak in a list"""
+    # linear search solution with Big O of O(n)
+    """
     list_len = len(list_of_integers) - 1
     start = 1
     if list_len < 0:
@@ -23,3 +25,19 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     if list_of_integers[list_len] > list_of_integers[-2]:
         return list_of_integers[list_len]
+    """
+
+    # binary search solution with Big O of O(log(n))
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return list_of_integers[left]
