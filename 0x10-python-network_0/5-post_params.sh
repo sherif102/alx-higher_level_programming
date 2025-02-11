df7a05f96a63ba6f981608302e02d96a24302b35
hyperbayse <hyperbayse@gmail.com>
2025-02-11 09:48:02 +0100

    correction on -d tag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request and customized data
-curl -sX POST -d "email=test@gmail.com" "subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
