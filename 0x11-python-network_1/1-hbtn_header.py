4d0dcd07a25f17fe433dfaed998d807726debb04
hyperbayse <hyperbayse@gmail.com>
2025-02-14 19:53:08 +0100

    doc update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -18,6 +18,9 @@ import urllib.request
 from sys import argv
 
 
-with urllib.request.urlopen(argv[1]) as response:
-    """fetch the request-id of a url"""
-    print(response.headers["X-Request-Id"])
+if __name__ == "__main__":
+    url = argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        """fetch the request-id of a url"""
+        print(response.headers["X-Request-Id"])
