0a2e05ff3c9cffb048e096a2f24c7d15856700ed
hyperbayse <hyperbayse@gmail.com>
2025-02-15 10:54:18 +0100

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
         else:
             # print(res)
             print(f'[{res.get("id")}] {res.get("name")}')
-    except requests.exceptions:
+    except Exception:
         print("Not a valid JSON")
