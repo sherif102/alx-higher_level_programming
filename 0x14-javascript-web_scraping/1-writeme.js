0fc964ff83cec4d0b717263353225f9d82661a1d
hyperbayse <hyperbayse@gmail.com>
2025-03-11 00:11:07 +0100

    script that write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+const fs = require('fs');
+
+fs.writeFile(argv[2], argv[3], 'utf-8', function (err, file) {
+  if (err) throw err;
+  console.log(argv[3]);
+});
