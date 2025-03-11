ac876279e6835cfbb54cefc2064832a7e7317588
hyperbayse <hyperbayse@gmail.com>
2025-03-11 17:28:48 +0100

    correction2

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -11,7 +11,7 @@ async function writeFromUrl () {
 
   const body = text.body;
 
-  fs.writeFile(argv[3], body.trim(), 'utf-8', function (err, done) {
+  fs.writeFile(argv[3], body.trimStart(), 'utf-8', function (err, done) {
     if (err) throw err;
   });
 }
