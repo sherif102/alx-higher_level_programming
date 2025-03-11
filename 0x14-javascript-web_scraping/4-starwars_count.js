29c8bf7ec2939fa6a5db4ec74cd946d762436992
hyperbayse <hyperbayse@gmail.com>
2025-03-11 16:50:34 +0100

    correction 1

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,24 +6,30 @@ const { promisify } = require('util');
 const promisedRequest = promisify(request);
 
 async function characterCounter () {
-  const text = await promisedRequest(`${argv[2]}`);
+  try {
+    const text = await promisedRequest(`${argv[2]}`);
 
-  const body = JSON.parse(text.body);
+    const body = JSON.parse(text.body);
 
-  let counter = 0;
-  const characterList = [];
-  for (const result of body.results) {
-    for (const character of result.characters) {
-      characterList.push(promisedRequest(character));
+    let counter = 0;
+    const characterList = [];
+    for (const result of body.results) {
+      for (const character of result.characters) {
+        if (character.slice(-3) === '18/') {
+          characterList.push(promisedRequest(character));
+        }
+      }
     }
-  }
 
-  const characters = await Promise.all(characterList);
+    const characters = await Promise.all(characterList);
 
-  for (const char of characters) {
-    if (JSON.parse(char.body).name === 'Wedge Antilles') { counter++; }
+    for (const char of characters) {
+      if (JSON.parse(char.body).name === 'Wedge Antilles') { counter++; }
+    }
+    console.log(counter);
+  } catch (error) {
+    console.error('');
   }
-  console.log(counter);
 }
 
 characterCounter();
